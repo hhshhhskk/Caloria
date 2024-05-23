2d4de9aff6360d192dc49ed0675e500a17eff571
hhshhhskk <hhshhhskk@naver.com>
2024-05-23 19:37:23 +0900

    feat: 푸터 추가 및 파이어베이스 연동

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
 
 export const metadata: Metadata = {
   title: "Caloria",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         {/* <Header /> */}
         {children}
+        <Footer />
       </body>
     </html>
   );
--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,9 +1,29 @@
+"use client";
+
 import React from "react";
-import styles from "../../styles/common/Footer.module.css";
+import styles from "../../styles/common/footer.module.css";
 import Image from "next/image";
 
 const Footer: React.FC = () => {
-  return <footer className={styles.footer}></footer>;
+  const category = ["home", "레시피", "게시판", "마이페이지"];
+
+  return (
+    <footer className={styles.footer}>
+      {category.map((item, idx) => {
+        return (
+          <div
+            key={idx}
+            className={styles.item}
+            onClick={() => {
+              console.log(item);
+            }}
+          >
+            {item}
+          </div>
+        );
+      })}
+    </footer>
+  );
 };
 
 export default Footer;
